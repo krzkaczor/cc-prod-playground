84e67e1fe2096afca2c39432f7b720a480244575
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-01-03 15:14:22 +0100

    do a change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 class App extends Component {
   onClick = () => {
     alert("AAAAAAAA this button does nothing!");
+    alert("AAAAAAAA this button does nothing!");
   };
 
   render() {
